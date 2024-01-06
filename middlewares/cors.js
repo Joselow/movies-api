eb632c7c9b6ab62557c7b29cd7daba79b85b2755
Joselow <Joselow>
2024-01-05 22:48:39 -0500

    interface to update finished

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,31 +1,28 @@
 import cors from 'cors'
+import dotenv from 'dotenv'
 
-const ACCEPTED_ORIGINS = [
+dotenv.config()
+const ACCEPTED_ORIGINS_VALUES = [
   'http://127.0.0.1:5500',
   'http://localhost:8080'
-  // add others
 ]
-// const corsOptions = {
-//   origin: (origin, callback) => {
-//     if (ACCEPTED_ORIGINS.includes(origin)) {
-//       return callback(null, true)
-//     } else if (!origin) {
-//       return callback(null, true)
-//     } else {
-//       const error = new Error('Not allowed by CORS')
-//       callback(error)
-//     }
-//   }
-// }
+
+const ACCEPTED_ORIGINS = process.env.ORIGIN_ACCEPTED || ACCEPTED_ORIGINS_VALUES
+
+console.log(ACCEPTED_ORIGINS)
 
 export const corsMiddleware = ({ acceptedOtigins = ACCEPTED_ORIGINS } = {}) => cors({
   origin: (origin, callback) => {
+    if (acceptedOtigins === '*') {
+      return callback(null, true) // 1: error, 2: success
+    }
     if (acceptedOtigins.includes(origin)) {
       return callback(null, true) // 1: error, 2: success
     } else if (!origin) {
       return callback(null, true)
     } else {
       const error = new Error('Not allowed by CORS')
+      console.log('ERROR: ---- Not allowed by CORS')
       callback(error)
     }
   }
--- models/mysql/connection.js
+++ models/mysql/connection.js
@@ -1,11 +1,13 @@
 import mysql from 'mysql2/promise'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const config = {
-  host: 'localhost',
-  user: 'root',
-  port: 3306,
-  password: '',
-  database: 'moviesdb'
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  port: parseInt(process.env.DB_PORT, 10) || 3306,
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_DATABASE || 'moviesdb'
 }
 
 let connection
